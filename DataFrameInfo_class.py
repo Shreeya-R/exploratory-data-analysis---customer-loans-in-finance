a6b685bacb7e134984a28cf6ba57969a9bb4b395
Shreeya Rathod <shreeya.rathod1@gmail.com>
2023-11-15 21:55:16 +0000

    Creates and begins to build the DataFrameInfo class

---
+++ DataFrameInfo_class.py
@@ -0,0 +1,9 @@
+# %%
+class DataFrameInfo:
+    '''
+    This class extracts information from the DataFrame and it's columns.
+    '''
+    def __init__(self):
+        '''
+        See help(DataFrameInfo) for accurate signature.
+        '''
