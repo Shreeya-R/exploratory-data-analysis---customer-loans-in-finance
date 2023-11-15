e909df0ac9f89d4fe14a5ca52bc069e2b61b2ac1
Shreeya Rathod <shreeya.rathod1@gmail.com>
2023-11-15 19:47:06 +0000

    Includes checking of the loan_data in order for correct cleaning of columns

--- familiarise_with_data.py
+++ familiarise_with_data.py
@@ -14,12 +14,77 @@ loan_data.shape
 # 54,231 rows
 # 43 columns
 # %%
+# Look at sample of size 30 of the data
+loan_data.sample(30)
+# %%
 # Describe data
-loan_data.describe
-loan_data.info
-loan_data.sample
+loan_data.info()
 
 # All of the above appear to give more or less the same information.
 # %%
+# Check data types of the data
+loan_data.dtypes
 
+# Change:
+# term from object -> timedelta64
+# grade from object -> string
+# subgrade from object -> string
+# employment_length from object -> timedelta64
+# home_ownership from object -> category 
+# verification_status from object -> category 
+# issue_date from object -> datetime64
+# loan_staus from object -> category (cut down some categories)
+# payment_plan from object -> boolean (yes & no values)
+# %%
+# Check home_ownership values are categorical
+print(loan_data['home_ownership'].nunique())
+print(loan_data['home_ownership'].unique())
+
+# has 5 unique values
+# unique values are 'MORTGAGE', 'RENT', 'OWN', 'OTHER', 'NONE'
+# %%
+# Check verification_status values are categorical
+print(loan_data['verification_status'].nunique())
+print(loan_data['verification_status'].unique())
+
+# has 3 unique values
+# unique values are 'Not Verified' 'Source Verified' 'Verified'
+# %%
+# Check loan_status values are categorical
+print(loan_data['loan_status'].nunique())
+print(loan_data['loan_status'].unique())
+
+# has 9 unique values
+# unique values are 'Current' 'Fully Paid' 'Charged Off' 'Late (31-120 days)' 'In Grace Period' 'Late (16-30 days)' 'Default' 'Does not meet the credit policy. Status:Fully Paid' 'Does not meet the credit policy. Status:Charged Off'
+# %%
+print(loan_data['payment_plan'].nunique())
+print(loan_data['payment_plan'].unique())
+
+# this is a boolean variable
+# %%
+print(loan_data['purpose'].nunique())
+print(loan_data['purpose'].unique())
+
+# has 14 unique values
+# unique values are 'credit_card' 'debt_consolidation' 'home_improvement' 'small_business' 'renewable_energy' 'major_purchase' 'other' 'moving' 'car' 'medical' 'house' 'vacation' 'wedding' 'educational'
+# %%
+print(loan_data['earliest_credit_line'])
+
+# %%
+print(loan_data['application_type'].nunique())
+print(loan_data['application_type'].unique())
+
+# only has 1 value, so doesn't add any information for us
+# %%
+print(loan_data['term'].nunique())
+print(loan_data['term'].unique())
+# %%
+print(loan_data['policy_code'].nunique())
+print(loan_data['policy_code'].unique())
+
+# only has 1 value, so doesn't add any information for us
+# %%
+print(loan_data['employment_length'].nunique())
+print(loan_data['employment_length'].unique())
+print(loan_data['employment_length'].dtype)
 # %%
