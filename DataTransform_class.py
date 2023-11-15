746be402a247dd1de616fd673f529d3070d9e22d
Shreeya Rathod <shreeya.rathod1@gmail.com>
2023-11-15 11:37:23 +0000

    Creates methods to transform columns to the correct data types

--- DataTransform_class.py
+++ DataTransform_class.py
@@ -1,4 +1,8 @@
 # %%
+import pandas as pd
+loan_data = pd.read_csv('loan_payment.csv')
+loan_data.head()
+# %%
 class DataTransform:
     '''
     This class converts the columns in the data to the correct format.
@@ -9,7 +13,38 @@ class DataTransform:
         See help(DataTransform) for accurate signature.
         '''
 
-    def 
+    def object_to_datetime():
         '''
+        This function converts object data types to datetime64 data types.
+
+        Returns:
+
+        '''
+        loan_data['issue_date','earliest_credit_line','last_payment_date','next_payment_date', 'last_credit_pull_date'] = loan_data['issue_date','earliest_credit_line','last_payment_date','next_payment_date', 'last_credit_pull_date'].apply(pd.to_datetime)
         
+    def object_to_boolean():
         '''
+        This function converts object data types to boolean data types.
+
+        Returns:
+
+        '''
+        loan_data['issue_date','earliest_credit_line','last_payment_date','next_payment_date', 'last_credit_pull_date'] = loan_data['issue_date','earliest_credit_line','last_payment_date','next_payment_date', 'last_credit_pull_date'].astype('bool')
+
+    def object_to_category():
+        '''
+        This function converts object data types to category data types.
+
+        Returns:
+
+        '''
+loan_data['issue_date','earliest_credit_line','last_payment_date','next_payment_date', 'last_credit_pull_date'] = loan_data['issue_date','earliest_credit_line','last_payment_date','next_payment_date', 'last_credit_pull_date'].astype('category')
+
+    def object_to_timedelta():
+        '''
+        This function converts object data types to timedelta64 data types.
+
+        Returns:
+
+        '''
+        loan_data['issue_date','earliest_credit_line','last_payment_date','next_payment_date', 'last_credit_pull_date'] = loan_data['issue_date','earliest_credit_line','last_payment_date','next_payment_date', 'last_credit_pull_date'].apply(pd.to_timedelta)
